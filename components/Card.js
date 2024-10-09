fafc9995361f8657b93eddab64c0d5685c37d725
Patrick <patrickyambrick@gmail.com>
2024-10-09 19:58:46 -0400

    Adjust responsive styling

--- components/Card.js
+++ components/Card.js
@@ -1,51 +1,63 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
-import styles from '../styles/Card.module.scss'
+import styles from '../styles/Card.module.scss';
 
 export default function Card(props) {
-    const redWords = ['book'];
+	const redWords = ['book'];
 
-    const handleRedWords = (data) => {
-        const dataArray = data.split(" ");
-        dataArray.map((word, index) => {
-            let wordArray = word.split('');
-            let hadPunctuation = false;
-            if (wordArray.indexOf('.') !== -1) { 
-                hadPunctuation = '.';
-                wordArray.pop();
-                word = wordArray.join('');
-            }
-            if (redWords.indexOf(word.toLowerCase()) !== -1) {
-                    dataArray.splice(index, 1, <span style={{color:'red'}}>{hadPunctuation ? word + hadPunctuation: word}</span>) 
-            } 
-        })
-        return dataArray.join(" ");
-    }
-    const renderChoices = () => {
-        if (props.choices) {
-            return (
-                <div>
-                <h3 className={styles.choicesTitle}>{handleRedWords(props.prompt)}</h3>
-                <ul className={styles.choicesList}>
-                {props.choices.map((choice, index) => (<li key={index} onClick={() => props.handleChoice(choice.number)} className={styles.choice}>{handleRedWords(choice.text)}</li>))}
-                </ul>
-                </div>
-            )
-        } else {
-            return (
-                <h3 className={styles.choicesTitle}>{props.prompt}</h3>
-            )
-        }
-    }
-    return (
-        <div className={styles.card}>
-            <div className={styles.imageContainer}>
-                <Image height={379} width={300} src={props.img} alt={props.alt}/>
-            </div>
-            <p className={styles.description}>
-                {handleRedWords(props.description)}
-            </p>
-            {renderChoices()}
-        </div>
-    )
+	const handleRedWords = (data) => {
+		const dataArray = data.split(' ');
+		dataArray.map((word, index) => {
+			let wordArray = word.split('');
+			let hadPunctuation = false;
+			if (wordArray.indexOf('.') !== -1) {
+				hadPunctuation = '.';
+				wordArray.pop();
+				word = wordArray.join('');
+			}
+			if (redWords.indexOf(word.toLowerCase()) !== -1) {
+				dataArray.splice(
+					index,
+					1,
+					<span style={{ color: 'red' }}>
+						{hadPunctuation ? word + hadPunctuation : word}
+					</span>
+				);
+			}
+		});
+		return dataArray.join(' ');
+	};
+	const renderChoices = () => {
+		if (props.choices) {
+			return (
+				<div>
+					<h3 className={styles.choicesTitle}>
+						{handleRedWords(props.prompt)}
+					</h3>
+					<ul className={styles.choicesList}>
+						{props.choices.map((choice, index) => (
+							<li
+								key={index}
+								onClick={() => props.handleChoice(choice.number)}
+								className={styles.choice}
+							>
+								{handleRedWords(choice.text)}
+							</li>
+						))}
+					</ul>
+				</div>
+			);
+		} else {
+			return <h3 className={styles.choicesTitle}>{props.prompt}</h3>;
+		}
+	};
+	return (
+		<div className={styles.card}>
+			<div className={styles.imageContainer}>
+				<Image height={379} width={280} src={props.img} alt={props.alt} />
+			</div>
+			<p className={styles.description}>{handleRedWords(props.description)}</p>
+			{renderChoices()}
+		</div>
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -16,110 +16,216 @@ import tailSonicHappy from '../public/TailSonicHappy.png';
 import healthyFruit from '../public/HealthyFruit.jpg';
 import someCandy from '../public/SomeCandy.png';
 import sonicSick from '../public/SonicSick.jpg';
+//fix this; there has to be a better way
+
 //--------------------//
-import Head from 'next/head'
+import Head from 'next/head';
 import Restart from '../components/Restart';
-import Card from '../components/Card'
-import styles from '../styles/Home.module.scss'
-import {useState} from 'react'
+import Card from '../components/Card';
+import styles from '../styles/Home.module.scss';
+import { useState } from 'react';
 
 export default function Home() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const titleText = "Sonic's Adventure";
+	const [currentPage, setCurrentPage] = useState(1);
+	const titleText = "Sonic's Adventure";
 
+	const pages = {
+		1: {
+			image: sonicBed,
+			alt: 'Sonic the Hedgehog asleep.',
+			text: 'Sonic sleeps.',
+			choices: [{ text: '>', number: 2 }],
+		},
+		2: {
+			image: alarmClock,
+			alt: 'An alarm clock.',
+			text: 'Sonic gets up.',
+			choices: [
+				{ text: 'Sonic gets mad.', number: '1a' },
+				{ text: 'Sonic smiles.', number: '1b' },
+			],
+		},
+		//--------------------//
+		'1a': {
+			image: sonicMad,
+			alt: 'Sonic the Hedgehog angry.',
+			text: 'Sonic is mad.',
+			choices: [
+				{ text: 'Sonic calms down.', number: '1b' },
+				{ text: 'Sonic stays mad.', number: '2a-b' },
+			],
+		},
+		'1b': {
+			image: sonicSmiles,
+			alt: 'Sonic the Hedgehog smiling.',
+			text: 'Sonic starts his day.',
+			choices: [
+				{ text: 'Sonic eats food.', number: '2b-a' },
+				{ text: 'Sonic runs to school.', number: '3b' },
+			],
+		},
+		//--------------------//
+		'2b-a': {
+			image: sonicFood,
+			alt: 'Sonic the Hedgehog with a chili dog.',
+			text: 'Sonic eats food.',
+			prompt: 'What does Sonic eat?',
+			choices: [
+				{ text: 'Sonic eats fruit.', number: '2b-a-a' },
+				{ text: 'Sonic eats candy.', number: '2b-a-b' },
+			],
+		},
+		'2b-a-a': {
+			image: healthyFruit,
+			alt: 'Smiling cartoon fruits.',
+			text: 'Sonic eats fruit.',
+			prompt: 'Now Sonic is ready to go to school.',
+			choices: [{ text: '>', number: '3b' }],
+		},
+		'2b-a-b': {
+			image: someCandy,
+			alt: 'Cartoon candies.',
+			text: 'Sonic eats candy.',
+			prompt: "Sonic's belly hurts.",
+			choices: [{ text: '>', number: '3c' }],
+		},
+		//--------------------//
+		//TODO: change options to reflect the removal of "3a"
+		'3b': {
+			image: sonicToSchool,
+			alt: 'Sonic the Hedgehog running with bag in hand.',
+			text: 'Sonic runs to school.',
+			prompt: 'What does Sonic see?',
+			choices: [
+				{ text: 'Amy looks sad.', number: '3b-a' },
+				{ text: 'Tails says hi.', number: '3b-b' },
+			],
+		},
+		'3c': {
+			image: sonicSick,
+			alt: 'Sonic the Hedgehog feeling unwell.',
+			text: 'Sonic feels sick.',
+			choices: [{ text: '>', number: 'final' }],
+		},
+		//--------------------//
+		'3b-a': {
+			image: amySad,
+			alt: 'Amy Rose crying.',
+			text: 'Amy is sad.',
+			prompt: 'What does Sonic do?',
+			choices: [
+				{ text: 'Sonic is kind to Amy.', number: '3b-a-a' },
+				{ text: 'Sonic does not know what to do.', number: '3b-a-b' },
+			],
+		},
+		'3b-a-a': {
+			image: sonicFlower,
+			alt: 'Sonic the Hedgehog giving Amy a flower.',
+			text: 'Sonic gives Amy a flower.',
+			prompt: 'See how Amy feels.',
+			choices: [{ text: '>', number: '4a' }],
+		},
+		'3b-a-b': {
+			image: tailsAmy,
+			alt: 'Tails consoling a sad Amy.',
+			text: 'Tails says nice things to Amy.',
+			prompt: 'See how Amy feels.',
+			choices: [{ text: '>', number: '4a' }],
+		},
+		//--------------------//
+		'3b-b': {
+			image: tailsHi,
+			alt: 'Tails waving to you.',
+			text: 'Tails says hi.',
+			prompt: 'What do Tails and Sonic do?',
+			choices: [
+				{ text: 'They read a book.', number: '3b-b-a' },
+				{ text: 'They run.', number: '3b-b-b' },
+			],
+		},
+		'3b-b-a': {
+			image: tailsBook,
+			alt: 'Tails reading a book.',
+			text: 'Sonic and Tails read a book.',
+			prompt: 'See how they feel.',
+			choices: [{ text: '>', number: '5b' }],
+		},
+		'3b-b-b': {
+			image: tailSonicRun,
+			alt: 'Sonic running while carrying Tails, both smiling.',
+			text: 'Sonic and Tails go for a run.',
+			prompt: 'See how they feel.',
+			choices: [{ text: '>', number: '5b' }],
+		},
+		//--------------------//
+		'4a': {
+			image: amySmile,
+			alt: 'Amy Rose smiling.',
+			text: 'Amy feels well.',
+			prompt: 'Sonic is glad Amy feels well.',
+			choices: [{ text: '>', number: '5a' }],
+		},
+		//--------------------//
+		'5a': {
+			image: sonicSmiles,
+			alt: 'Sonic the Hedgehog smiling.',
+			text: 'Sonic smiles.',
+			prompt: 'Sonic has had a good day.',
+			choices: [{ text: '>', number: 'final' }],
+		},
+		'5b': {
+			image: tailSonicHappy,
+			alt: 'Tails and Sonic smiling together.',
+			text: 'They feel well.',
+			prompt: 'Sonic has had a good day.',
+			choices: [{ text: '>', number: 'final' }],
+		},
+		//--------------------//
+		final: {
+			image: sonicBed,
+			alt: 'Sonic the Hedgehog asleep.',
+			text: 'Sonic goes to sleep.',
+			prompt: 'The end.',
+			choices: false,
+		},
+	};
 
-  const pages = {
-    1: {image: sonicBed, alt: "Sonic the Hedgehog asleep.", text:"Sonic sleeps.", choices: [{text: ">", number: 2}]},
-    2: {image: alarmClock, alt: "An alarm clock.", text: "Sonic gets up.", choices: [
-      {text: "Sonic gets mad.", number: "1a"}, {text: "Sonic smiles.", number: "1b"}
-    ]},
-    //--------------------//
-    "1a": {image: sonicMad, alt: "Sonic the Hedgehog angry.", text: "Sonic is mad.", choices: [
-      {text: "Sonic calms down.", number: "1b"}, {text: "Sonic stays mad.", number: "2a-b"}
-    ]},
-    "1b": {image: sonicSmiles, alt: "Sonic the Hedgehog smiling.", text: "Sonic starts his day.", choices: [
-      {text: "Sonic eats food.", number: "2b-a"}, {text: "Sonic runs to school.", number: "3b"}
-    ]},
-    //--------------------//
-    "2b-a": {image: sonicFood, alt: "Sonic the Hedgehog with a chili dog.", text: "Sonic eats food.", prompt: "What does Sonic eat?", choices: [
-      {text: "Sonic eats fruit.", number: "2b-a-a"}, {text: "Sonic eats candy.", number: "2b-a-b"}
-    ]},
-    "2b-a-a": {image: healthyFruit, alt: "Smiling cartoon fruits.", text: "Sonic eats fruit.", prompt: "Now Sonic is ready to go to school.", choices: [
-      {text: ">", number: "3b"}
-    ]},
-    "2b-a-b": {image: someCandy, alt: "Cartoon candies.", text: "Sonic eats candy.", prompt: "Sonic's belly hurts.", choices: [
-      {text: ">", number: "3c"}
-    ]},
-    //--------------------//
-    //TODO: change options to reflect the removal of "3a"
-    "3b": {image: sonicToSchool, alt: "Sonic the Hedgehog running with bag in hand.", text: "Sonic runs to school.", prompt: "What does Sonic see?", choices: [
-      {text: "Amy looks sad.", number: "3b-a"}, {text: "Tails says hi.", number: "3b-b"}
-    ]},
-    "3c": {image: sonicSick, alt: "Sonic the Hedgehog feeling unwell.", text: "Sonic feels sick.", choices: [
-      {text: ">", number: "final"}
-    ]},
-    //--------------------//
-    "3b-a": {image: amySad, alt: "Amy Rose crying.", text: "Amy is sad.", prompt: "What does Sonic do?", choices: [
-      {text: "Sonic is kind to Amy.", number: "3b-a-a"}, {text: "Sonic does not know what to do.", number: "3b-a-b"}
-    ]},
-    "3b-a-a": {image: sonicFlower, alt: "Sonic the Hedgehog giving Amy a flower.", text: "Sonic gives Amy a flower.", prompt: "See how Amy feels.", choices: [
-      {text: ">", number: "4a"}
-    ]},
-    "3b-a-b": {image: tailsAmy, alt: "Tails consoling a sad Amy.", text: "Tails says nice things to Amy.", prompt: "See how Amy feels.", choices: [
-      {text: ">", number: "4a"}
-    ]},
-    //--------------------//
-    "3b-b": {image: tailsHi, alt: "Tails waving to you.", text: "Tails says hi.", prompt: "What do Tails and Sonic do?", choices: [
-      {text: "They read a book.", number: "3b-b-a"}, {text: "They run.", number: "3b-b-b"}
-    ]},
-    "3b-b-a": {image: tailsBook, alt: "Tails reading a book.", text: "Sonic and Tails read a book.", prompt: "See how they feel.", choices: [
-      {text: ">", number: "5b"}
-    ]},
-    "3b-b-b": {image: tailSonicRun, alt: "Sonic running while carrying Tails, both smiling.", text: "Sonic and Tails go for a run.", prompt: "See how they feel.", choices: [
-      {text: ">", number: "5b"}
-    ]},
-    //--------------------//
-    "4a": {image: amySmile, alt: "Amy Rose smiling.", text: "Amy feels well.", prompt: "Sonic is glad Amy feels well.", choices: [
-      {text: ">", number: "5a"}
-    ]},
-    //--------------------//
-    "5a": {image: sonicSmiles, alt: "Sonic the Hedgehog smiling.", text: "Sonic smiles.", prompt: "Sonic has had a good day.", choices: [
-      {text: ">", number: "final"}
-    ]},
-    "5b": {image: tailSonicHappy, alt: "Tails and Sonic smiling together.", text: "They feel well.", prompt: "Sonic has had a good day.", choices: [
-      {text: ">", number: "final"}
-    ]},
-    //--------------------//
-    "final": {image: sonicBed, alt: "Sonic the Hedgehog asleep.", text: "Sonic goes to sleep.", prompt: "The end.", choices: false}
-  }
-  
+	const handleChoice = (choiceNumber) => {
+		if (
+			Object.keys(pages).indexOf(choiceNumber) !== -1 ||
+			choiceNumber === 1 ||
+			choiceNumber === 2
+		) {
+			//if the choice exists, select it
+			setCurrentPage(choiceNumber);
+		} else {
+			//if the choice does not exist, go to the end
+			setCurrentPage('final');
+		}
+	};
 
-  const handleChoice = (choiceNumber) => {
-    if ((Object.keys(pages).indexOf(choiceNumber) !== -1) || (choiceNumber === 1) || (choiceNumber === 2)) {
-      //if the choice exists, select it
-      setCurrentPage(choiceNumber);
-    } else {
-      //if the choice does not exist, go to the end
-      setCurrentPage("final")
-    }
-  }
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>{titleText}</title>
+				<meta name="description" content="Generated by create next app" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>{titleText}</title>
-        <meta name="description" content="Generated by create next app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          <em>{titleText}</em>
-        </h1>
-        <Card prompt={pages[currentPage].prompt || "What does Sonic do next?"} choices={pages[currentPage].choices} description={pages[currentPage].text} alt={pages[currentPage].alt} img={pages[currentPage].image} handleChoice={handleChoice}/>
-        <Restart handleChoice={handleChoice} />
-      </main>
-    </div>
-  )
+			<main className={styles.main}>
+				<h1 className={styles.title}>
+					<em>{titleText}</em>
+				</h1>
+				<Card
+					prompt={pages[currentPage].prompt || 'What does Sonic do next?'}
+					choices={pages[currentPage].choices}
+					description={pages[currentPage].text}
+					alt={pages[currentPage].alt}
+					img={pages[currentPage].image}
+					handleChoice={handleChoice}
+				/>
+				<Restart handleChoice={handleChoice} />
+			</main>
+		</div>
+	);
 }
